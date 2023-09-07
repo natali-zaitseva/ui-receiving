e4082f760bddaadd8cb27dfb34a9d985b001f8e1
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-09-07 16:43:24 +0500

    remove: bigTest in favour of jest configs

--- test/bigtest/interactors/consts.js
+++
@@ -1,2 +0,0 @@
-export const TIMEOUT = 5000;
-export const MODAL_SELECTOR = '[class*=modal---]';
--- test/bigtest/interactors/index.js
+++
@@ -1,7 +0,0 @@
-export { default as PieceFormInteractor } from './PieceFormInteractor';
-export { default as TitleDetailsInteractor } from './TitleDetailsInteractor';
-export { default as TitleFormInteractor } from './TitleFormInteractor';
-export { default as TitleReceiveInteractor } from './TitleReceiveInteractor';
-export { default as TitlesListInteractor } from './TitlesListInteractor';
-export { default as TitleUnreceiveInteractor } from './TitleUnreceiveInteractor';
-export * from './consts';
